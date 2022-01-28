49e8d6364ed8a68c22039494c07312c917fc70f1
George Gomes <georgevag@protonmail.com>
2022-01-28 20:41:02 -0300

    Fix file names and folder hierarchy and update version

--- GGDevelopmentKit/Sources/ViewController/GGAlertableViewController.swift
+++ GGDevelopmentKit/Sources/ViewController/GGAlertableViewController.swift
@@ -32,33 +32,3 @@ public extension GGAlertableViewController {
     }
 }
 
-public extension UIView {
-    static private let loadingTag = 2456
-    
-    func showLoading() {
-        let wallView = UIView.init(frame: bounds)
-        wallView.tag = UIView.loadingTag
-        wallView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
-        
-        var indicator: UIActivityIndicatorView
-        if #available(iOS 13.0, *) {
-            indicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
-        } else {
-            indicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.whiteLarge)
-        }
-        indicator.startAnimating()
-        indicator.center = wallView.center
-
-        DispatchQueue.main.async {
-            wallView.addSubview(indicator)
-            self.addSubview(wallView)
-        }
-    }
-    
-    func removeLoading() {
-        DispatchQueue.main.async {
-            self.subviews.filter({$0.tag == UIView.loadingTag}).forEach({$0.removeFromSuperview()})
-        }
-    }
-}
-
