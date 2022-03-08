32b325e07ef9186f0b7a3bb1147d1fd3567b8798
George Gomes <georgevag@protonmail.com>
2022-03-08 19:06:17 -0300

    Setup GGAutolayout

---
+++ GGDevelopmentKit/Sources/Components/Code/AutoLayout/GGAutoLayout.swift
@@ -0,0 +1,9 @@
+//
+//  GGAutoLayout.swift
+//  GGDevelopmentKit
+//
+//  Created by George Vilnei Arboite Gomes on 08/03/22.
+//  Copyright © 2022 George Gomes. All rights reserved.
+//
+
+import Foundation
